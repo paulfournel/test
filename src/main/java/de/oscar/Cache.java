a5a2e46cc3d51a852bd65fe35fb0c39aac34d18d
Paul Fournel <paul.fournel@zalando.de>
2018-11-15 22:02:23 +0100

    more infos on cache

--- src/main/java/de/oscar/Cache.java
+++ src/main/java/de/oscar/Cache.java
@@ -4,11 +4,19 @@ public class Cache {
     private final String lonLat;
     private final String hint;
     private final String difficulty;
+    private final String terrain;
+    private final String size;
+    private final String cacheName;
+    private final String user;
 
-    public Cache(String lonLat, String hint, String difficulty) {
+    public Cache(String cacheName, String user, String lonLat, String hint, String difficulty, String terrain, String size) {
+        this.cacheName = cacheName;
+        this.user = user;
         this.lonLat = lonLat;
         this.hint = hint;
         this.difficulty = difficulty;
+        this.terrain = terrain;
+        this.size = size;
     }
 
     public String getLonLat() {
@@ -21,5 +29,21 @@ public class Cache {
 
     public String getDifficulty() {
         return difficulty;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getCacheName() {
+        return cacheName;
+    }
+
+    public String getUser() {
+        return user;
     }
 }
--- src/main/java/de/oscar/GeoCachingProxy.java
+++ src/main/java/de/oscar/GeoCachingProxy.java
@@ -9,9 +9,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 
 @Service
 public class GeoCachingProxy {
+    private final static HashMap<String, String> CODE = makeCodes();
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -20,11 +23,20 @@ public class GeoCachingProxy {
     public Cache getCacheById(String cacheId) {
         driver.get("http://coord.info/" + cacheId);
 
+        String cacheName = driver.findElementById("ctl00_ContentBody_CacheName").getText();
         String lonLat = driver.findElementById("uxLatLon").getText();
-        String hint = driver.findElementById("ctl00_ContentBody_EncryptionKey").getText();
-        String[] difficulty = driver.findElementByClassName("CacheStarLabels").findElement(By.tagName("img")).getAttribute("src").split("/");
+        String hint = decrypt(driver.findElementById("div_hint").getText());
+        String user = driver.findElementById("ctl00_ContentBody_mcd1").findElement(By.tagName("a")).getText();
+        String difficulty = getImageName("ctl00_ContentBody_uxLegendScale");
+        String terrain = getImageName("ctl00_ContentBody_Localize12");
+        String size = getImageName("ctl00_ContentBody_size");
 
-        return new Cache(lonLat, hint.split("\n")[3], difficulty[difficulty.length - 1]);
+        return new Cache(cacheName, user, lonLat, hint, difficulty, terrain, size);
+    }
+
+    private String getImageName(String id) {
+        String[] difficulty = driver.findElementById(id).findElement(By.tagName("img")).getAttribute("src").split("/");
+        return difficulty[difficulty.length - 1].replace(".gif", "").replace("stars", "").replace("_", ".");
     }
 
     @Scheduled(fixedDelay = 1500000)
@@ -48,5 +60,45 @@ public class GeoCachingProxy {
         driver.getKeyboard().pressKey("310598");
         driver.findElement(new By.ById("SignIn")).click();
         logger.info("Finished login");
+    }
+
+    private static String decrypt(String converted) {
+        String res = "";
+        for (int i = 0; i < converted.length(); i++) {
+            String c = Character.toString(converted.charAt(i));
+            res += CODE.get(c.toUpperCase()).toLowerCase();
+        }
+        return res;
+    }
+
+    private static HashMap<String, String> makeCodes() {
+        HashMap<String, String> codes = new HashMap<>();
+        codes.put("A", "N");
+        codes.put("B", "O");
+        codes.put("C", "P");
+        codes.put("D", "Q");
+        codes.put("E", "R");
+        codes.put("F", "S");
+        codes.put("G", "T");
+        codes.put("H", "U");
+        codes.put("I", "V");
+        codes.put("J", "W");
+        codes.put("K", "X");
+        codes.put("L", "Y");
+        codes.put("M", "Z");
+        codes.put("N", "A");
+        codes.put("O", "B");
+        codes.put("P", "C");
+        codes.put("Q", "D");
+        codes.put("R", "E");
+        codes.put("S", "F");
+        codes.put("T", "G");
+        codes.put("U", "H");
+        codes.put("V", "I");
+        codes.put("W", "J");
+        codes.put("X", "K");
+        codes.put("Y", "L");
+        codes.put("Z", "M");
+        return codes;
     }
 }
